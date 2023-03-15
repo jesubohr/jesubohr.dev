67dcaf0688ad690c611e91c9c0ef7522ebdc261b
Jesús Borrero <jesufer2018@hotmail.com>
2023-03-14 19:40:15 -0500

    Separate projects list

---
+++ src/lib/projects.ts
@@ -0,0 +1,53 @@
+import type { Project } from "../types"
+
+export const PROJECTS: Project[] = [
+  {
+    name: "Todoist",
+    description: "An app that allows you to create, check and delete tasks.",
+    image: "/projects/todoist.webp",
+    screenshot: "/projects/screenshots/todoist.jpeg",
+    link: "https://todoist-planner.surge.sh",
+    repository: "https://github.com/jesubohr/ToDoist"
+  },
+  {
+    name: "RecipeAI",
+    description: "A beautiful and powerful recipe recommender app powered by AI.",
+    image: "/projects/recipeai.webp",
+    screenshot: "/projects/screenshots/recipeai.jpeg",
+    link: "https://recipe-ai.vercel.app/",
+    repository: "https://github.com/jesubohr/RecipeAI"
+  },
+  {
+    name: "ChatGPT",
+    description: "A clone of ChatGPT made for practice purposes.",
+    image: "/projects/chatgpt.webp",
+    screenshot: "/projects/screenshots/chatgpt.jpeg",
+    link: "https://github.com/jesubohr/ChatGPT",
+    repository: "https://github.com/jesubohr/ChatGPT"
+  },
+  {
+    name: "GenPass",
+    description: "A simple generator of secure passwords for daily use.",
+    image: "/projects/genpass.webp",
+    screenshot: "/projects/screenshots/genpass.jpeg",
+    link: "https://altgenpass.vercel.app/",
+    repository: "https://github.com/jesubohr/GenPass"
+  },
+  {
+    name: "Scales2Care",
+    description:
+      "A PWA to help health professionals to apply certain risk scales on hospitalized patients.",
+    image: "/projects/scales2care.webp",
+    screenshot: "/projects/screenshots/scales2care.jpeg",
+    link: "https://scales2care.live",
+    repository: "https://github.com/jesubohr/scales2care"
+  },
+  {
+    name: "Color Palette",
+    description:
+      "Simple app that allows users to create color palettes and share them.",
+    image: "/projects/palettio.webp",
+    screenshot: "/projects/screenshots/palettio.jpeg",
+    link: "https://palettio-color.surge.sh"
+  },
+]
