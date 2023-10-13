d6bfd213f779f7a7d77bb1c057e1992c2e72c711
Jesús Borrero <jesufer2018@hotmail.com>
2023-10-13 00:45:42 -0500

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://jesubohr-dev.vercel.app/',
   output: 'server',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  integrations: [mdx(), sitemap(), react(), tailwind()],
+  adapter: vercel()
 });
