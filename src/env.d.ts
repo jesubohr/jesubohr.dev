cef532e07c7b2886600d889c593e799aaa59a2c9
Jesús Borrero <jesufer2018@hotmail.com>
2024-02-20 10:29:10 -0500

    Add ImportMetaEnv interface to env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PERSONAL_EMAIL: string
+  readonly SENDER_EMAIL: string
+  readonly SENDER_PASSWORD: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
